4cf43e16035ce9da8b23190c5eeb4e610f5c530c
Yurii Myronov <xmyron00@stud.fit.vutbr.cz>
2023-04-11 11:45:53 +0200

    Component 2

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -20,11 +20,98 @@
       </div>
 
       <div class="component2">
+        <q-carousel
+          v-model="slide"
+          transition-prev="slide-right"
+          transition-next="slide-left"
+          swipeable
+          animated
+          ref="carousel"
+          infinite
+          style="background: transparent"
+        >
+          <q-carousel-slide :name="1" class="column no-wrap q-pb-lg">
+            <div class="row fit no-wrap">
+              <div class="col-3 full-height slide-item" style="padding-right: 10px; height: 400px">
+                <div class="bg-blue-grey-3 full-width" style="height: 50%"></div>
+                <div class="bg-white">
+                  <p style="font-weight: bold;">Titul Cykloskokan Do práce na kole 2022 získano Uherské Hradiště. Otrokovice vstupuji do Sině slávy</p>
+                  <p class="text-grey-6">27. záři 2022</p>
+                </div>
+              </div>
+              <div class="col-3 full-height slide-item" style="padding: 0 10px; height: 400px">
+                <div class="bg-blue-grey-3 full-width" style="height: 50%"></div>
+                <div class="bg-white">
+                  <p style="font-weight: bold;">Titul Cykloskokan Do práce na kole 2022 získano Uherské Hradiště. Otrokovice vstupuji do Sině slávy</p>
+                  <p class="text-grey-6">27. záři 2022</p>
+                </div>
+              </div><div class="col-3 full-height slide-item" style="padding: 0 10px; height: 400px">
+              <div class="bg-blue-grey-3 full-width" style="height: 50%"></div>
+              <div class="bg-white">
+                <p style="font-weight: bold;">V rámci Evropského týdne mobility odstartoval už 6. ročník Zářijové výzvy Do práce na kole</p>
+                <p class="text-grey-6">14. záři 2022</p>
+              </div>
+              </div><div class="col-3 full-height slide-item" style="padding-left: 10px; height: 400px">
+                <div class="bg-blue-grey-3 full-width" style="height: 50%"></div>
+                <div class="bg-white">
+                  <p style="font-weight: bold;">V rámci Evropského týdne mobility odstartoval už 6. ročník Zářijové výzvy Do práce na kole</p>
+                  <p class="text-grey-6">14. záři 2022</p>
+                </div>
+              </div>
+            </div>
+          </q-carousel-slide>
+          <q-carousel-slide :name="2" class="column no-wrap q-pb-lg">
+            <div class="row fit no-wrap">
+              <div class="col-3 full-height slide-item" style="padding-right: 10px; height: 400px">
+                <div class="bg-blue-grey-3 full-width" style="height: 50%"></div>
+                <div class="bg-white">
+                  <p style="font-weight: bold;">Titul Cykloskokan Do práce na kole 2022 získano Uherské Hradiště. Otrokovice vstupuji do Sině slávy</p>
+                  <p class="text-grey-6">27. záři 2022</p>
+                </div>
+              </div>
+              <div class="col-3 full-height slide-item" style="padding: 0 10px; height: 400px">
+                <div class="bg-blue-grey-3 full-width" style="height: 50%"></div>
+                <div class="bg-white">
+                  <p style="font-weight: bold;">Titul Cykloskokan Do práce na kole 2022 získano Uherské Hradiště. Otrokovice vstupuji do Sině slávy</p>
+                  <p class="text-grey-6">27. záři 2022</p>
+                </div>
+              </div><div class="col-3 full-height slide-item" style="padding: 0 10px; height: 400px">
+              <div class="bg-blue-grey-3 full-width" style="height: 50%"></div>
+              <div class="bg-white">
+                <p style="font-weight: bold;">V rámci Evropského týdne mobility odstartoval už 6. ročník Zářijové výzvy Do práce na kole</p>
+                <p class="text-grey-6">14. záři 2022</p>
+              </div>
+            </div><div class="col-3 full-height slide-item" style="padding-left: 10px; height: 400px">
+              <div class="bg-blue-grey-3 full-width" style="height: 50%"></div>
+              <div class="bg-white">
+                <p style="font-weight: bold;">V rámci Evropského týdne mobility odstartoval už 6. ročník Zářijové výzvy Do práce na kole</p>
+                <p class="text-grey-6">14. záři 2022</p>
+              </div>
+            </div>
+            </div>
+          </q-carousel-slide>
 
+          <template v-slot:control>
+            <q-carousel-control class="q-gutter-md">
+              <q-btn
+                push dense color="black" text-color="white" label="Navštívit blog" style="left: -200%" class="q-pa-sm"
+              />
+              <q-btn
+                push round dense color="white" text-color="black" icon="fa fa-arrow-left"
+                @click="$refs.carousel.previous()"
+              />
+              <q-btn
+                push round dense color="white" text-color="black" icon="fa fa-arrow-right"
+                @click="$refs.carousel.next()"
+              />
+            </q-carousel-control>
+          </template>
+
+        </q-carousel>
       </div>
 
-      <q-banner class="bg-blue-grey-2 relative-position">
-        <p class="section q-ma-none">Sledujte nás na sociálních sítích</p>
+      <q-banner class="bg-blue-grey-2 relative-position q-my-lg">
+        <p class="section q-ma-none q-pa-md">Sledujte nás na sociálních sítích</p>
         <div class="row absolute-right q-my-auto content-center">
             <q-icon clickable tag="a" target="_blank" href="" name="fa-brands fa-facebook" size="20px" class="q-pa-sm q-mx-sm text-black"/>
             <q-icon clickable tag="a" target="_blank" href="" name="fa-brands fa-instagram" size="20px" class="q-pa-sm q-mx-sm text-black"/>
@@ -38,7 +125,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import conf from 'src/myconfiguration.json';
 import { setCssVar } from 'quasar';
 
@@ -53,6 +140,12 @@ export default defineComponent({
       backgroundImage: 'background-image: url("src/images/' + conf.backgroundImage + '")',
       title: conf.titleImage,
       message: conf.messageImage,
+    }
+  },
+
+  setup () {
+    return {
+      slide: ref(1),
     }
   }
 });
@@ -70,7 +163,8 @@ export default defineComponent({
 
   div.component1 > div{
     max-width: 1200px;
-    margin: auto;
+    margin-right: auto;
+    margin-left: auto;
   }
 
   .section{
@@ -99,6 +193,18 @@ export default defineComponent({
   .q-banner .q-icon{
     border: 2px solid black;
     text-decoration: none;
+  }
+
+  .slide-item div:first-child{
+    border-radius: 5px 5px 0 0;
+  }
+
+  .slide-item div:last-child{
+    border-radius: 0 0 15px 15px;
+    border-style: none solid solid solid;
+    border-color: $grey-3;
+    border-width: 2px;
+    padding: 5px;
   }
 
 </style>
